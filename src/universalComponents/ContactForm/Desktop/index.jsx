63d090da4111fec1b04a91cfb1e4d88a108460a9
justincecc <justinceccarelli@gmail.com>
2023-11-07 13:40:51 -0700

    added successful notification

--- src/universalComponents/ContactForm/Desktop/index.jsx
+++ src/universalComponents/ContactForm/Desktop/index.jsx
@@ -1,4 +1,14 @@
-import { Box, TextField, Button } from "@mui/material";
+import {
+        Box,
+        TextField,
+        Button,
+        Dialog,
+        DialogTitle,
+        DialogContent,
+        DialogContentText,
+        DialogActions,
+        CircularProgress,
+    } from "@mui/material";
 import { useState } from 'react';
 import axios from 'axios';
 function ContactForm() {
@@ -7,10 +17,24 @@ function ContactForm() {
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
     const sendEmail = async () => {
-        await axios.post(`${HBC_API}/sendInquiryEmail`, { firstName, lastName, email, message });
-        await axios.post(`${HBC_API}/sendOutreachEmail`, { to: email });
+        setLoading(true);
+        const res = await axios.post(`${HBC_API}/sendInquiryEmail`, { firstName, lastName, email, message });
+        if (res.status === 201) {
+            await axios.post(`${HBC_API}/sendOutreachEmail`, { to: email });
+            setLoading(false);
+            setMessage('');
+            setEmail('');
+            setLastName('');
+            setFirstName('');
+            setOpen(true);
+        }
     };
+    const handleClose = () => {
+        setOpen(false);
+    }
 
     return (
         <div style={{ marginLeft: 'auto' }}>
@@ -22,48 +46,73 @@ function ContactForm() {
                 noValidate
                 autoComplete="off"
             >
-                <div style={{ flexDirection: 'columm', width: '100%'}}>
-                    <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '2vh', justifyContent: 'space-evenly' }}>
+                    <div style={{ flexDirection: 'columm', width: '100%'}}>
+                        <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '2vh', justifyContent: 'space-evenly' }}>
+                            <TextField
+                                fullWidth
+                                label="First Name"
+                                sx={{ paddingRight: '10px' }}
+                                value={ firstName }
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                            <TextField
+                                fullWidth
+                                label="Last Name"
+                                value={ lastName }
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
                         <TextField
                             required
+                            label="Email"
                             fullWidth
-                            label="First Name"
-                            sx={{ paddingRight: '10px' }}
-                            value={ firstName }
-                            onChange={(e) => setFirstName(e.target.value)}
+                            sx={{ marginBottom: '2vh' }}
+                            value={ email }
+                            error={!email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                         <TextField
                             required
-                            fullWidth
-                            label="Last Name"
-                            value={ lastName }
-                            onChange={(e) => setLastName(e.target.value)}
+                            fullWidth 
+                            multiline
+                            rows={4}
+                            label="Your inquiry..."
+                            value={ message }
+                            error={!message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            sx={{ marginBottom: '2vh' }}
                         />
+                        {!loading ?
+                        <>
+                            <Button variant="contained" disabled={ !email || !message } onClick={sendEmail} >
+                                Submit Request
+                            </Button>
+                        </>
+                        :
+                        <>
+                            <CircularProgress
+                                size={24}
+                                sx={{
+                                color: '#5688e0',
+                                }}
+                            />
+                        </>
+                        }
                     </div>
-                    <TextField
-                        required
-                        label="Email"
-                        fullWidth
-                        sx={{ marginBottom: '2vh' }}
-                        value={ email }
-                        error={!email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <TextField
-                        required
-                        fullWidth 
-                        multiline
-                        rows={4}
-                        label="Your inquiry..."
-                        value={ message }
-                        error={!message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        sx={{ marginBottom: '2vh' }}
-                    />
-                    <Button variant="contained" disabled={!email || !message } onClick={sendEmail} >
-                        Submit Request
-                    </Button>
-                </div>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <DialogTitle>Success!</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Thank you for reaching out to us! Your inquiry has been successfully received. We will review it and get back to you as soon as possible. If you have any urgent questions, please feel free to contact us directly at hello@hbcreations.io
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} fullWidth variant="contained">Ok</Button>
+                    </DialogActions>
+                </Dialog>
             </Box>
         </div>
     );
--- src/universalComponents/ContactForm/Mobile/index.jsx
+++ src/universalComponents/ContactForm/Mobile/index.jsx
@@ -1,16 +1,41 @@
-import { Box, TextField, Button } from "@mui/material";
+import {
+        Box,
+        TextField,
+        Button,
+        Dialog,
+        DialogContent,
+        DialogContentText,
+        DialogActions,
+        DialogTitle,
+        CircularProgress,
+    } from "@mui/material";
 import { useState } from 'react';
+
 import axios from 'axios';
 function SiteTabs() {
     const HBC_API = 'https://api.sh4pesdevelopment.com/api';
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
-    const [email, setEmail] = useState();
-    const [message, setMessage] = useState();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
     const sendEmail = async () => {
-        await axios.post(`${HBC_API}/sendInquiryEmail`, { firstName, lastName, email, message });
-        await axios.post(`${HBC_API}/sendOutreachEmail`, { to: email });
+        setLoading(true);
+        const res = await axios.post(`${HBC_API}/sendInquiryEmail`, { firstName, lastName, email, message });
+        if (res.status === 201) {
+            await axios.post(`${HBC_API}/sendOutreachEmail`, { to: email });
+            setLoading(false);
+            setMessage('');
+            setEmail('');
+            setLastName('');
+            setFirstName('');
+            setOpen(true);
+        }
     };
+    const handleClose = () => {
+        setOpen(false);
+    }
     return (
         <div style={{ marginLeft: 'auto' }}>
             <Box
@@ -58,10 +83,37 @@ function SiteTabs() {
                         onChange={(e) => setMessage(e.target.value)}
                         sx={{ marginBottom: '10px' }}
                     />
-                    <Button fullWidth variant="contained" disabled={!email || !message } onClick={sendEmail} >
-                        Submit Request
-                    </Button>
+                    {!loading ?
+                        <>
+                            <Button fullWidth variant="contained" disabled={!email || !message }  onClick={sendEmail} >
+                                Submit Request
+                            </Button>
+                        </>
+                        :
+                        <>
+                            <CircularProgress
+                                size={24}
+                                sx={{
+                                color: '#5688e0',
+                                }}
+                            />
+                        </>
+                        }
                 </div>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <DialogTitle>Success!</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Thank you for reaching out to us! Your inquiry has been successfully received. We will review it and get back to you as soon as possible. If you have any urgent questions, please feel free to contact us directly at hello@hbcreations.io
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} fullWidth variant="contained">Ok</Button>
+                    </DialogActions>
+                </Dialog>
             </Box>
         </div>
     );
