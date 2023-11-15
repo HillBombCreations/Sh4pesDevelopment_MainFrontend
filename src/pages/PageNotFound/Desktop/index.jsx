90564fdf8e8a70a577e923233ab05cf9c169a0ae
justincecc <justinceccarelli@gmail.com>
2023-11-14 22:52:41 -0700

    Fixed 404

--- src/pages/PageNotFound/Desktop/index.jsx
+++ src/pages/PageNotFound/Desktop/index.jsx
@@ -11,16 +11,19 @@ export default class Login extends Component {
     return (
       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', paddingRight: '15vw', paddingLeft: '15vw' }}>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-          <h2 style={{ width: '480px', fontSize: '128px', color: '#3780FF'}}>404</h2>
-          <p style={{ width: '480px', fontSize: '24px', marginTop: '-10vh'}}>Yikes!</p>
-          <p style={{ width: '480px', fontSize: '24px', marginTop: '-1vh', marginBottom: '10vh'}}>Looks like you wandered away a little too far, make you way back by clicking the button</p>
-          <Button
-                variant="contained"
-                onClick={() => window.location.replace('/')}
-                sx={{ width: '50%', bgcolor: '#3780FF' }}
-          >
-              Back To Home
-          </Button>
+          <h2 style={{ width: '480px', fontSize: '128px', color: '#3780FF', marginBottom: '0'}}>404</h2>
+          <p style={{ width: '480px', fontSize: '24px', marginBottom: '0'}}>Yikes!</p>
+          <p style={{ width: '480px', fontSize: '24px', marginBottom: '5vh'}}>Looks like you wandered away a little too far, make you way back by clicking the button</p>
+          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+            <img src="/assets/duck.png" alt="Sh4pes" style={{ width: '340px' }} />
+            <Button
+                  variant="contained"
+                  onClick={() => window.location.replace('/')}
+                  sx={{ width: '50%', height: '7vh', bgcolor: '#3780FF' }}
+            >
+                Back To Home
+            </Button>
+          </div>
         </div>
         <div style={{ position: 'absolute', bottom: '0' }}>
           <div style={{ display: 'flex', justifyContent: 'center' }}>
