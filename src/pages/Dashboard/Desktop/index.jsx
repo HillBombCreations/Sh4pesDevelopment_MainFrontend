c3f66f9718199c91ab77c9e68fc4668ea5e72e6f
justincecc <justinceccarelli@gmail.com>
2023-11-17 23:19:34 -0700

    Nav bar groovy

--- src/pages/Dashboard/Desktop/index.jsx
+++ src/pages/Dashboard/Desktop/index.jsx
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types';
 import {
   Box, CssBaseline, AppBar, Toolbar,
   Drawer, List, ListItem, ListItemButton, ListItemIcon,
-  ListItemText, Divider, Container
+  ListItemText, Divider, IconButton
 } from "@mui/material";
 import {
-  AttachMoney, Logout, Support, Dashboard
+  AttachMoney, Logout, Support, Dashboard, Menu,
+  ChevronLeft,
 } from '@mui/icons-material';
 import { useState } from 'react'
 import cookieFns from '../../../utils/cookieFns';
@@ -17,67 +18,127 @@ const drawerWidth = 240;
 
 function DesktopLanding() { 
   const [pageType, setPageType] = useState('dashboard');
+  const [open, setOpen] = useState(true);
   const { eatCookie } = cookieFns();
+
   const Component = () => {
     if (pageType === 'billing') return <BillingPage />;
     if (pageType === 'dashboard') return <DashboardPage />;
     if (pageType === 'account') return <AccountPage />;
     if (pageType === 'support') return <SupportPage />;
   };
+
   return (
     <div>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar elevation={0} position="fixed" sx={{ backgroundColor: '#3780FF', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-          <Container maxWidth="100%">
-            <Toolbar disableGutters>
+        <AppBar
+        elevation={0}
+        position="fixed"
+        sx={{
+          backgroundColor: '#3780FF',
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          marginLeft: open ? drawerWidth : '',
+          width: open ? `calc(100% - ${drawerWidth}px)` : '100%',
+        }}>
+            <Toolbar>
+              <IconButton
+                color="#fffff"
+                aria-label="open drawer"
+                onClick={() => setOpen(true)}
+                edge="start"
+                sx={{
+                  marginRight: 5,
+                  ...(open && { display: 'none' }),
+                }}
+              >
+                <Menu />
+              </IconButton>
               <img src="/assets/sh4pes_blue-bg_with-logo.png" alt="Sh4pes Banner" style={{ width: '150px', height: 'auto' }} />
             </Toolbar>
-          </Container>
         </AppBar>
         <Drawer
           variant="permanent"
+          open={open}
           sx={{
-            width: drawerWidth,
+            width: open ? drawerWidth : '4vw',
             flexShrink: 0,
-            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+            whiteSpace: 'nowrap',
+            boxSizing: 'border-box',
+            overflowX: 'hidden',
+            [`& .MuiDrawer-paper`]: { width: open ? drawerWidth : '4vw', boxSizing: 'border-box', whiteSpace: 'nowrap', overflowX: 'hidden' },
           }}
         >
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', overflowX: 'hidden'}}>
+            <IconButton onClick={() => setOpen(false)} sx={{ marginTop: '1vh', marginRight: '1vw', marginBottom: 0, backgroundColor: '#3780FF' }}>
+              <ChevronLeft />
+            </IconButton>
+          </div>
           <Toolbar />
-          <Box sx={{ overflow: 'auto', paddingX: '15px', marginTop: '50px' }}>
+          <Box sx={{ flexDirection: 'column', display: 'flex', alignContent: 'center', justifyContent: 'center' }}>
             <List>
-              <ListItem key='dashboard' disablePadding>
-                <ListItemButton onClick={() => setPageType('dashboard')}>
+              <ListItem key='dashboard' disablePadding sx={{ color: pageType === 'dashboard' ? '#3780FF' : ''  }}>
+                <ListItemButton
+                  onClick={() => setPageType('dashboard')}
+                  sx={{ minHeight: 48 }}
+                >
                   <ListItemIcon>
-                    <Dashboard />
+                    <Dashboard sx={{ color: pageType === 'dashboard' ? '#3780FF' : '' }}/>
                   </ListItemIcon>
-                  <ListItemText primary='Dashboard' />
+                  {
+                    open ?
+                    <ListItemText primary='Dashboard' /> :
+                    null
+                  }
                 </ListItemButton>
               </ListItem>
-              <ListItem key='billing' disablePadding>
-                <ListItemButton onClick={() => setPageType('billing')}>
+              <ListItem key='billing' disablePadding sx={{ color: pageType === 'billing' ? '#3780FF' : ''  }}>
+                <ListItemButton
+                  onClick={() => setPageType('billing')}
+                  sx={{ minHeight: 48 }}
+                >
                   <ListItemIcon>
-                    <AttachMoney />
+                    <AttachMoney sx={{ color: pageType === 'billing' ? '#3780FF' : '' }}/>
                   </ListItemIcon>
-                  <ListItemText primary='Billing' />
+                  {
+                    open ?
+                    <ListItemText primary='Billing' /> :
+                    null
+                  }
                 </ListItemButton>
               </ListItem>
             </List>
-            <Divider sx={{ backgroundColor: '#d9d8d8bf', width: '90%', marginX: 'auto' }} />
-            <ListItem key='support' disablePadding>
-              <ListItemButton onClick={() =>  setPageType('support')}>
+            <Divider sx={{ backgroundColor: '#d9d8d8bf', width: open ? '90%' : '100%' }} />
+            <ListItem key='support' disablePadding sx={{ color: pageType === 'support' ? '#3780FF' : ''  }}>
+              <ListItemButton
+                onClick={() =>  setPageType('support')}
+                sx={{ minHeight: 48 }}
+              >
                 <ListItemIcon>
-                  <Support />
+                  <Support sx={{ color: pageType === 'support' ? '#3780FF' : '' }}/>
                 </ListItemIcon>
-                <ListItemText primary='Support' />
+                {
+                  open ?
+                  <ListItemText primary='Support' /> :
+                  null
+                }
               </ListItemButton>
             </ListItem>
             <ListItem key='logout' disablePadding>
-              <ListItemButton onClick={eatCookie}>
+              <ListItemButton
+                onClick={() => eatCookie}
+                sx={{
+                  minHeight: 48,
+                }}
+              >
                 <ListItemIcon>
                   <Logout />
                 </ListItemIcon>
-                <ListItemText primary='Log out' />
+                {
+                  open ?
+                  <ListItemText primary='Log out' /> :
+                  null
+                }
               </ListItemButton>
             </ListItem>
           </Box>
