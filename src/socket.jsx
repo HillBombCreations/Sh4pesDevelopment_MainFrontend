ecc54a2b6997790d6857a73f3f8fea2cab2e705a
Hunter Kreshock <hkreshock@gmail.com>
2023-11-19 01:38:15 -0700

    update route for socket

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://api.sh4pesdevelopment.com', { path: '/api' });
+const socket = io('https://api.sh4pesdevelopment.com', { path: '/api/socket.io' });
 
 export { socket };
