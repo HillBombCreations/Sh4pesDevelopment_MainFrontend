813cdce3d29de0a22a50878814839465ce77883f
justincecc <justinceccarelli@gmail.com>
2023-11-18 22:58:12 -0700

    Support desktop done

--- src/pages/Dashboard/Desktop/Support/index.jsx
+++ src/pages/Dashboard/Desktop/Support/index.jsx
@@ -37,55 +37,57 @@ const SupportPage = memo(function SupportPage({ email }) {
     });
   };
     return (
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', paddingRight: '15vw', paddingLeft: '15vw'}}>
-        <div style={{ display: 'flex', flexDirection: 'column', marginTop: '30vh', alignItems: 'center', paddingRight: '5vw' }}>
-          <p style={{ width: '480px', fontSize: '24px', marginBottom: 0}}>We apologize for any inconveniences you may be facing. Let us know whats going on and we will respond to your message as soon as we can.</p>
-        </div>
-        <Box component="form" sx={{  display: 'flex', flexDirection: 'row', marginTop: '30vh' }} noValidate autoComplete="off">
-          <Card raised sx={{ bgcolor: '#fffff', paddingY: '4vh', paddingX: '2vw', width: '30vw'}}>
-            {     
-                accountError === 'success' ? 
-                  <Alert severity="success" sx={{ marginX: '2.6vw', marginBottom: '2vh' }}>
-                    Support request successfully sent
-                  </Alert> 
-                :
-                accountError === 'error' ?
-                <Alert severity="error" sx={{ marginX: '2.6vw', marginBottom: '2vh' }}>
-                  There was a problem submiting support, please reachout directly at support@hbcreations.io
-                </Alert>
-                :
-                null
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', alignContent: 'center', paddingRight: '15vw', paddingLeft: '15vw'}}>
+        <h2 style={{ width: '480px', fontSize: '24px', color: '#3780FF', marginBottom: '15vh'}}>Contact Support</h2>
+        <div style={{ display: 'flex', flexDirection: 'row'}}>
+          <img src="/assets/supportImage.png" alt="Sh4pes" style={{ width: '380px', marginRight: '10vw' }} />
+          <Box component="form" sx={{  display: 'flex', flexDirection: 'row' }} noValidate autoComplete="off">
+            <Card raised sx={{ bgcolor: '#fffff', paddingY: '4vh', paddingX: '2vw', width: '30vw'}}>
+              {     
+                  accountError === 'success' ? 
+                    <Alert severity="success" sx={{ marginX: '2.6vw', marginBottom: '2vh' }}>
+                      Support request successfully sent
+                    </Alert> 
+                  :
+                  accountError === 'error' ?
+                  <Alert severity="error" sx={{ marginX: '2.6vw', marginBottom: '2vh' }}>
+                    There was a problem submiting support, please reachout directly at support@hbcreations.io
+                  </Alert>
+                  :
+                  null
 
-            }
-            <TextField
-                required
-                fullWidth 
-                multiline
-                rows={4}
-                label="Your message..."
-                value={ message }
-                error={!message}
-                onChange={(e) => setMessage(e.target.value)}
-                sx={{ marginBottom: '2vh' }}
-            />
-            {!loading ?
-              <>
-                  <Button variant="contained" disabled={ !message } onClick={sendSupportEmail} >
-                      Submit Request
-                  </Button>
-              </>
-              :
-              <>
-                  <CircularProgress
-                      size={24}
-                      sx={{
-                      color: '#3780FF',
-                      }}
-                  />
-              </>
-            }
-          </Card>
-        </Box>
+              }
+              <p style={{ fontSize: '18px' }}>We apologize for any inconveniences you may be facing. Let us know whats going on and we will respond to your message as soon as we can.</p>
+              <TextField
+                  required
+                  fullWidth 
+                  multiline
+                  rows={4}
+                  label="Your message..."
+                  value={ message }
+                  error={!message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  sx={{ marginBottom: '2vh' }}
+              />
+              {!loading ?
+                <>
+                    <Button variant="contained" disabled={ !message } onClick={sendSupportEmail} >
+                        Submit Request
+                    </Button>
+                </>
+                :
+                <>
+                    <CircularProgress
+                        size={24}
+                        sx={{
+                        color: '#3780FF',
+                        }}
+                    />
+                </>
+              }
+            </Card>
+          </Box>
+        </div>
       </div>
     );
 })
