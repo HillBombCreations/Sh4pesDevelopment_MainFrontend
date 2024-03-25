ed07808ed09d19b8322da7c76cf73606a09e6029
justincecc <justinceccarelli@gmail.com>
2024-03-25 10:22:43 -0700

    Testing login to see if we get cookie

--- src/pages/Login/Desktop/index.jsx
+++ src/pages/Login/Desktop/index.jsx
@@ -48,7 +48,8 @@ export default class Login extends Component {
       )
       .then((res) => {
         if (res.status === 200) {
-          document.cookie = `user=${JSON.stringify(res.data)}; path=/`;
+          console.log(document.cookie);
+          // document.cookie = `user=${JSON.stringify(res.data)}; path=/`;
           window.location.replace("/");
         } else {
           this.setState({ loading: false });
