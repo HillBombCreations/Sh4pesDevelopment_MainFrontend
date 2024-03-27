65586344fe9ed4c421379b6ae1df2293f63f74af
Hunter Kreshock <hkreshock@gmail.com>
2024-03-27 00:01:56 -0700

    file restructure, new routing for authed paths

--- src/universalComponents/wrappers/withAuth.jsx
+++ src/universalComponents/wrappers/withAuth.jsx
@@ -31,8 +31,7 @@ export default function withAuth(ComponentToProtect, pathname) {
         }
       }).catch(err => {
         console.error(err);
-        // UPDATE: this.setState({ loading: false, redirectBool: true });
-        this.setState({ loading: false });
+        this.setState({ loading: false, redirectBool: true });
       });
 
       // set event listener so we can resize responsively
@@ -48,7 +47,7 @@ export default function withAuth(ComponentToProtect, pathname) {
         return null;
       }
       if (redirectBool) {
-        // UPDATE: window.location.replace('/login');
+        window.location.replace('/login');
         return <CircularProgress />;
       }
 
